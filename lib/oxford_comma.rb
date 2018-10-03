105616012c337cfb224c57a258fc55bcc0b40cfe
Megan Farley <megfarley4@gmail.com>
2018-10-03 20:47:43 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,8 @@
 def oxford_comma(array)
-a
+  string = ""
+if array.size == 0 || array.size == 1
+  string = array.to_s
+else
+  string = "#{array[0..array.size-2].join(", ")}, and  #{array[-1]}"
+end
 end
