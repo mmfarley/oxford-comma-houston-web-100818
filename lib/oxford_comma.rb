5c228b0f464834529278f797624652bfae99ef49
Megan Farley <megfarley4@gmail.com>
2018-10-03 20:49:44 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,6 +2,8 @@ def oxford_comma(array)
   string = ""
 if array.size == 0 || array.size == 1
   string = array.to_s
+  elsif array.size == 2
+  string = array.join(" and ")
 else
   string = "#{array[0..array.size-2].join(", ")}, and  #{array[-1]}"
 end
