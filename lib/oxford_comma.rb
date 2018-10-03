976fcc5480713c09bb212517960185c2f7f68f3f
Megan Farley <megfarley4@gmail.com>
2018-10-03 20:51:10 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  string = ""
+if array.size == 0 || array.size == 1
+  string = array.join
+  elsif array.size == 2
+  string = array.join(" and ")
+else
+  string = "#{array[0..array.size-2].join(", ")}, and #{array[-1]}"
+end
+string
 end
